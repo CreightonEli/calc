f38238adcd569e030eef9bcba40b9fdb43301e18
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2024-08-12 03:05:16 -0400

    finishing up

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // constants // 
 // theme toggle
+const rootEl = document.querySelector("html")
 const toggleBtn = document.getElementById("toggle-btn");
 const toggleSwitch = document.getElementById("toggle-switch");
 // calc elements
@@ -9,43 +10,84 @@ const screenNum = document.getElementById("screen-num");
 let numbers = [];
 let operators = [];
 let displayString = "0";
+let decimal = false;
+let theme = localStorage.getItem('colorTheme');
 
 // functions //
-function display() {screenNum.innerText = displayString;}
+
+// theme switcher
+function switchTheme() {
+    switch (theme) {
+        case 'theme-1':
+            theme = 'theme-2';
+            break;
+        case 'theme-2':
+            theme = 'theme-3';
+            break;
+        case 'theme-3':
+            theme = 'theme-1';
+            break;
+        default:
+            theme = 'theme-2';
+    }
+    
+    localStorage.setItem('colorTheme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+}
+            
+// calculator
+function display() {
+    displayString = displayString.toString().replace(/,/g, "");
+    let parts = displayString.split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    displayString = parts.join(".");
+    screenNum.innerText = displayString;
+}
 
 function concat(input) {
     displayString = String(displayString)
-    if (displayString === "0" && input === ".") {}
+    if (displayString === "0" && input === "."){}
     else if (displayString === "0") {
         displayString = "";
     }
+
+    // check if decimal
+    if (decimal === true && input === ".") {
+        input = "";
+    }
+    
+    if (input === "." && decimal === false) {
+        decimal = true;
+    }
+
     displayString += input;
     display();
+
 }
 
 function add() {
-    numbers.push(Number(displayString));
+    numbers.push(Number(displayString.replace(/,/g, "")));
     operators.push("+");
     displayString = "0";
     display();
 }
 
 function sub() {
-    numbers.push(Number(displayString));
+    numbers.push(Number(displayString.replace(/,/g, "")));
     operators.push("-");
     displayString = "0";
     display();
 }
 
 function mult() {
-    numbers.push(Number(displayString));
+    numbers.push(Number(displayString.replace(/,/g, "")));
     operators.push("*");
     displayString = "0";
     display();
 }
 
 function div() {
-    numbers.push(Number(displayString));
+    numbers.push(Number(displayString.replace(/,/g, "")));
     operators.push("/");
     displayString = "0";
     display();
@@ -87,16 +129,30 @@ function reset() {
     numbers = [];
     operators = [];
     displayString = "0";
+    decimal = false;
     display();
 }
 
 function del() {
-    if (displayString === "0") {}
-    else {
+    if (displayString === "0") {} // if display string is just 0, do nothing
+    else { // delete
+        if (displayString.slice(-1) === ".") {
+            decimal = false;
+        }
         displayString = String(displayString);
         displayString = displayString.slice(0, -1);
+
+        if (displayString === ""){
+            displayString = "0";
+        }
+
         display();
     }
 }
 
-// document.addEventListener(KeyboardEvent)
+// Event listeners
+
+window.addEventListener('DOMContentLoaded', () => {
+    const theme = localStorage.getItem('colorTheme');
+    theme && document.documentElement.setAttribute('data-theme', theme);
+});
